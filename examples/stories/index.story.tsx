f39672e67ed46097e2c56684b4f938cfb250758e
foryuki <foryuki@outlook.com>
2018-11-04 15:33:15 +0800

    feat: release v1.0.0 OMG

--- examples/stories/index.story.tsx
+++ examples/stories/index.story.tsx
@@ -72,7 +72,7 @@ storiesOf('or-button', module).add(
         <h1>text is too long:</h1>
         <div className="long">
           <Button size="large" onClick={handleClick}>
-            TOO LOOOOOOOOOOOOOOOOOOOG
+            TOO LOOOOOOOOOOOOOOOOOG
           </Button>
         </div>
       </div>
